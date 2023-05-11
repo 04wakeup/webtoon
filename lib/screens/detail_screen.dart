776fe9450ccd145a1c4b15334984eee4fd352088
04wakeup <42279557+04wakeup@users.noreply.github.com>
2023-05-10 20:31:47 -0700

    episodes part1

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -44,49 +44,49 @@ class _DetailsScreenState extends State<DetailsScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: widget.id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 5,
-                          offset: const Offset(0, 0),
-                          color: Colors.black.withOpacity(0.5),
-                        )
-                      ]),
-                  child: Image.network(
-                    widget.thumb,
-                    headers: const {
-                      "User-Agent":
-                          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-                    },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: widget.id,
+                  child: Container(
+                    width: 250,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 5,
+                            offset: const Offset(0, 0),
+                            color: Colors.black.withOpacity(0.5),
+                          )
+                        ]),
+                    child: Image.network(
+                      widget.thumb,
+                      headers: const {
+                        "User-Agent":
+                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+                      },
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FutureBuilder(
-            future: webtoon,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: Column(
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
@@ -105,13 +105,36 @@ class _DetailsScreenState extends State<DetailsScreen> {
                         ),
                       ),
                     ],
-                  ),
-                );
-              }
-              return const Text('...');
-            },
-          )
-        ],
+                  );
+                }
+                return const Text('...');
+              },
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            FutureBuilder(
+                future: episodes,
+                builder: ((context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(children: [
+                      for (var episode in snapshot.data!)
+                        Container(
+                          child: Row(
+                            children: [
+                              Text(episode.title),
+                              const Icon(
+                                Icons.chevron_right_rounded,
+                              )
+                            ],
+                          ),
+                        )
+                    ]);
+                  }
+                  return Container();
+                }))
+          ],
+        ),
       ),
     );
   }
